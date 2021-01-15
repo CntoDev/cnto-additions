891a642bf5e190c935388129e3a098702ee4c28c
freghar <freghar@dummy.tld>
2021-01-15 18:35:19 +0100

    copy CNTO logo from cnto-additions-old and use it for the mod
    Signed-off-by: freghar <freghar@dummy.tld>

--- mod.cpp
+++ mod.cpp
@@ -2,3 +2,5 @@ name = "CNTO Additions";
 author = "Various Artists";
 hideName = 0;
 overview = "Small Arma 3 customizations created by the CNTO community for Serious (not Serious Sam) gameplay.";
+picture = "cnto_logo.paa";
+logo = "cnto_logo.paa";
