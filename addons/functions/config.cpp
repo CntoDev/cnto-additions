ec4164aed62f6696bfc25826ef25019318a6f505
Highway <milanimilivoj@gmail.com>
2021-01-31 13:47:37 +0100

    add performance monitoring function

---
+++ addons/functions/config.cpp
@@ -0,0 +1,19 @@
+/*
+ * performance monitoring functions
+ */
+class CfgPatches {
+    class cnto_functions {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {};
+    };
+};
+
+class CfgFunctions {
+    class cnto {
+        class performance {
+            file = "\cnto\additions\functions\performance";
+            class runTest;
+        };
+    };
+};
