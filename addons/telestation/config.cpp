2ce68a9be21fcff2f057417911e5aeb9c6203259
Seb <sebsmith90@gmail.com>
2021-02-04 14:13:36 +0000

    Initial commit

---
+++ addons/telestation/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches {
+    class cnto_telestation {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {
+
+        };
+    };
+};
+
+class CfgFunctions {
+    class cnto_telestation {
+        class all {
+            file = "\cnto\additions\telestation";
+        };
+    };
+};
