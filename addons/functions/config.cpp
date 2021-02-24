bfba48bf44f1671b71334b8da54190ffe11b6db1
freghar <freghar@dummy.tld>
2021-02-24 16:41:10 +0000

    add boxGuard to functions/loadout
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/functions/config.cpp
+++ addons/functions/config.cpp
@@ -13,6 +13,10 @@ class CfgPatches {
 
 class CfgFunctions {
     class cnto {
+        class loadout {
+            file = "\cnto\additions\functions\loadout";
+            class boxGuard;
+        };
         class performance {
             file = "\cnto\additions\functions\performance";
             class runTest;
