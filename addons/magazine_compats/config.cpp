e6798bb5fe47c89321b19ff1100cea9aa0b05d97
Seb <65898127+Seb105@users.noreply.github.com>
2021-05-14 23:10:17 +0100

    Explain where magazines come from

--- addons/magazine_compats/config.cpp
+++ addons/magazine_compats/config.cpp
@@ -1,24 +1,35 @@
 class CfgPatches {
     class cnto_magazine_compats {
         units[] = {};
-        weapons[] = {"Rifle_Base_F", "arifle_CTARS_base_F", "arifle_CTAR_base_F"};
+        weapons[] = {};
         requiredAddons[] = {
-            "A3_Weapons_F_Exp_Rifles_CTARS",
-            "rhs_c_weapons",
-            "cba_jam"
+            "A3_Weapons_F_Exp_Rifles_CTARS"
+            // "rhs_c_weapons",
+            // "cba_jam"
         };
     };
 };
 
-class CfgWeapons {
+class CfgWeapons 
+{
     class Rifle_Base_F;
     // Make CAR-95 able to use 5.45mm ammo.
     class arifle_CTARS_base_F: Rifle_Base_F
     {
-        magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL", "AK_545x39","CBA_545x39_AK","CBA_545x39_RPK"};
+        magazineWell[] += {
+            // RHS AK_545x39 Magazine well from "rhs_c_weapons"
+            "AK_545x39", 
+            // CBA Magazine wells from addon "cba_jam"
+            "CBA_545x39_AK", "CBA_545x39_RPK"
+        };
     };
     class arifle_CTAR_base_F: Rifle_Base_F
     {
-        magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL", "AK_545x39","CBA_545x39_AK","CBA_545x39_RPK"};
+        magazineWell[] += {
+            // RHS AK_545x39 Magazine well from "rhs_c_weapons"
+            "AK_545x39", 
+            // CBA Magazine wells from addon "cba_jam"
+            "CBA_545x39_AK", "CBA_545x39_RPK"
+        };
     };
 };
