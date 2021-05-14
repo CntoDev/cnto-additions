ebade4b67f834db9db1485e1bcdc03a2124d2278
Seb <65898127+Seb105@users.noreply.github.com>
2021-05-14 23:00:04 +0100

    Generalise addon

--- addons/magazine_compats/config.cpp
+++ addons/magazine_compats/config.cpp
@@ -1,5 +1,5 @@
 class CfgPatches {
-    class cnto_545_CAR95 {
+    class cnto_magazine_compats {
         units[] = {};
         weapons[] = {"Rifle_Base_F", "arifle_CTARS_base_F", "arifle_CTAR_base_F"};
         requiredAddons[] = {
@@ -12,6 +12,7 @@ class CfgPatches {
 
 class CfgWeapons {
     class Rifle_Base_F;
+    // Make CAR-95 able to use 5.45mm ammo.
     class arifle_CTARS_base_F: Rifle_Base_F
     {
         magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL", "AK_545x39","CBA_545x39_AK","CBA_545x39_RPK"};
