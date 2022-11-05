3c70dff4265abd37d7aec3f7f2cf0788e69a5932
freghar <freghar@dummy.tld>
2022-11-05 00:00:01 +0000

    ace_medical_tweaks: use wound probabilities estimating ACE 3.14
    This should revert the bruisepocalypse.
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/ace_medical_tweaks/config.cpp
+++ addons/ace_medical_tweaks/config.cpp
@@ -60,6 +60,65 @@ class ACE_Medical_StateMachine {
     };
 };
 
+class ACE_Medical_Injuries {
+    class wounds {
+        // make burns bleed a little bit, so they can be bandaged/stitched
+        // - unlike bruises, they don't produce other wounds that would bleed,
+        //   and are rare enough to where bleeding probably doesn't matter
+        class ThermalBurn {
+            bleeding = 0.01;
+        };
+    };
+    class damageTypes {
+        // generally less wounds per bullet
+        class bullet {
+            //thresholds[] = {{20, 10}, {4.5, 2}, {3, 1}, {0, 1}};
+            thresholds[] = {{20, 6}, {5, 1}, {0, 1}};
+            delete Contusion;
+        };
+        // notably less wounds per small explosive effect, but at least 1
+        class grenade {
+            //thresholds[] = {{20, 10}, {10, 5}, {4, 3}, {1.5, 2}, {0.8, 2}, {0.3, 1}, {0, 0}};
+            thresholds[] = {{20, 8}, {10, 3}, {4, 2}, {0.3, 1}, {0, 0}};
+            class Contusion { weighting[] = {{0.5, 0}, {0.35, 0.2}}; };
+        };
+        // a lot of wounds for big damage, but very few for small damage
+        class explosive {
+            //thresholds[] = {{20, 15}, {8, 7}, {2, 3}, {1.2, 2}, {0.4, 1}, {0,0}};
+            thresholds[] = {{20, 15}, {8, 4}, {0.4, 1}, {0, 0}};
+            class Contusion { weighting[] = {{0.5, 0}, {0.35, 0.2}}; };
+        };
+        // shrapnel - same idea as explosive, but less wounds overall
+        class shell {
+            //thresholds[] = {{20, 10}, {10, 5}, {4.5, 2}, {2, 2}, {0.8, 1}, {0.2, 1}, {0, 0}};
+            thresholds[] = {{20, 10}, {10, 3}, {4, 2}, {0.2, 1}, {0, 0}};
+            class Cut { weighting[] = {{0.7, 0}, {0.35, 1}}; };
+            class Contusion { weighting[] = {{0.5, 0}, {0.35, 0.2}}; };
+        };
+        class vehiclecrash {
+            delete Contusion;  // due to discrete weight ranges
+        };
+        class collision {
+            class Contusion { weighting[] = {{0.4, 0}, {0.2, 0.2}}; };
+        };
+        // fall damage - reduce overall bruise amount
+        class falling {
+            //thresholds[] = {{8, 4}, {1, 1}, {0.2, 1}, {0.1, 0.7}, {0, 0.5}};
+            thresholds[] = {{8, 3}, {1, 1}, {0.2, 1}, {0.1, 0.7}, {0, 0.5}};
+            class Contusion { weighting[] = {{0.4, 0}, {0.2, 0.2}}; };
+        };
+        // backblast - same idea as fall damage
+        class backblast {
+            //thresholds[] = {{1, 6}, {1, 5}, {0.55, 5}, {0.55, 2}, {0, 2}};
+            thresholds[] = {{1, 3}, {0.55, 2}, {0, 1}};
+            class Contusion { weighting[] = {{0.35, 0}, {0.35, 0.2}}; };
+        };
+        class punch {
+            class Contusion { weighting[] = {{0.35, 0}, {0.35, 0.2}}; };
+        };
+    };
+};
+
 /*
  * unconsciousness condition tweaks
  *
